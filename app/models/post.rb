f9503bc719dad6d0048246e11e96472b1182555a
adnapp <adam.napps@gmail.com>
2020-11-22 11:19:26 -0500

    commit tree 58edefda4158d11c29ff37f039604354bce1a4a4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :is_clickbait?
+
+    #define clickbait words in array
+    #check and see if they are conatined, if they are not containes, add error
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d*/i,
+        /Guess/i
+      ]
+
+    def is_clickbait?
+        if CLICKBAIT.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
+
+
+
+
 end
